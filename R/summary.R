f4a76dcffc36ce598d651f189a4bf57312f32d42
Paul Buerkner <paul.buerkner@gmail.com>
2019-10-11 09:18:14 +0200

    add 'summary' as an alias of 'summary_draws'

---
+++ R/summary.R
@@ -0,0 +1,14 @@
+#' @export
+summary.draws_matrix <- function(object, ...) {
+  summarise_draws(object, ...)
+}
+
+#' @export
+summary.draws_array <- function(object, ...) {
+  summarise_draws(object, ...)
+}
+
+#' @export
+summary.draws_data_frame <- function(object, ...) {
+  summarise_draws(object, ...)
+}
