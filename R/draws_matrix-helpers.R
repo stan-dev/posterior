c52df52c97eed862fb90f7d76d5dcddf54b68931
Paul Buerkner <paul.buerkner@gmail.com>
2019-10-27 20:45:16 +0100

    remove the right methods

--- R/draws_matrix-helpers.R
+++ R/draws_matrix-helpers.R
@@ -8,14 +8,10 @@ is_draws_matrix_like <- function(x) {
   is.matrix(x) || is.array(x) && length(dim(x)) == 2L
 }
 
-# TODO: do we want to support this '$' method at all?
-# select draws of a single parameter
 #' @export
-`$.draws_matrix` <- function(x, name) {
-  class(x) <- "matrix"
-  out <- x[, name, drop = FALSE]
-  # selectively drop the second dimension
-  dim(out) <- dim(x)[1]
-  dimnames(out) <- dimnames(x)[1]
+`[.draws_matrix` <- function(x, i, j, ..., drop = FALSE) {
+  # TODO: add a warning that 'drop' is ignored?
+  out <- NextMethod("[", drop = FALSE)
+  class(out) <- class(x)
   out
 }
