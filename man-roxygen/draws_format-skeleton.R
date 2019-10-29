a17314b04f6e23e6b9c94d7f35e470797ee65ebc
jgabry <jgabry@gmail.com>
2019-10-29 16:51:41 -0400

    add some comments to roxygen template to explain

--- man-roxygen/draws_format-skeleton.R
+++ man-roxygen/draws_format-skeleton.R
@@ -3,10 +3,21 @@
 #'
 #' @param x An object to convert to a `<%=draws_format%>` object.
 #' @param ... Arguments passed to individual methods (if applicable).
-#'
+
+
+# The @return template also uses a base_class template variable.
+# We could determine base_class just from sub("draws_", "", draws_format)
+# except that doesnt' work because draws_df isn't a df but a tibble, so I
+# added a base_class template variable.
+
 #' @return A `<%=draws_format%>` object, which has classes
 #' `c("<%=draws_format%>", "draws", "<%=base_class%>")`.
-#'
+
+
+# An example that can be used for all formats to demonstrate the structure, but
+# we can always add format-specific examples by also explicitly adding an
+# @examples section to the format-specific pages
+
 #' @examples
 #' x <- <%=paste0("as_", draws_format)%>(draws_eight_schools)
 #' class(x)
