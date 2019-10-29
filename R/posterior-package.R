0df1e1aad39a44c2f5817881763e6fc44a110b8f
jgabry <jgabry@gmail.com>
2019-10-29 14:17:16 -0400

    update posterior-package.R

--- R/posterior-package.R
+++ R/posterior-package.R
@@ -1,10 +1,18 @@
-#' Tools for working posterior distributions
-#'
-#' Efficiently convert between different representations of posterior samples
-#' and compute modern MCMC diagnostics.
+#' Tools for working with posterior (and prior) distributions
 #'
 #' @docType package
 #' @name posterior-package
+#' @aliases posterior
 #' @import checkmate
+#'
+#' @description The \pkg{posterior} package is intended to provide useful tools
+#'   for both users and developers of packages for fitting Bayesian models or
+#'   working with output from Bayesian models. The primary goals of the
+#'   package are to:
+#'   * Efficiently convert between many different useful representations of
+#'     draws (samples) from posterior or prior distributions.
+#'   * Provide various summaries of draws in convenient formats.
+#'   * Provide implementations of state of the art MCMC diagnostics without
+#'     requiring a dependency on packages like \pkg{rstan}.
 #'
 NULL
