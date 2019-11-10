3711dc2f28ff01016348ef1ebab355b792d8919a
Matthew Kay <matthew.kay@gmail.com>
2019-11-09 21:26:37 -0500

    better rvar print() and str()

--- R/rvar-print.R
+++ R/rvar-print.R
@@ -11,7 +11,7 @@
 #'
 #' @export
 print.rvar = function(x, ...) {
-  cat0("rvar<", ndraws(x), ">:\n")
+  cat0(rvar_type_abbr(x), ":\n")
   print(format(x, color = FALSE), quote = FALSE)
   invisible(x)
 }
@@ -19,28 +19,33 @@ print.rvar = function(x, ...) {
 #' @rdname print.rvar
 #' @export
 format.rvar = function(x, ..., color = FALSE) {
-  draws = field(x, 1)
-
-  if (is.numeric(draws) || is.logical(draws)) {
-    summary_dimensions = seq_len(length(dim(draws)) - 1)
-
-    .mean = apply(draws, summary_dimensions, mean)
-    .sd = apply(draws, summary_dimensions, sd)
-    out = format_mean_sd(.mean, .sd, color = color)
-
-    dim(out) = dim(draws)[summary_dimensions]
-    dimnames(out) = dimnames(draws)[summary_dimensions]
-    out
-  } else {
-    pillar::obj_sum(draws[[1]])
-  }
+  format_draws(field(x, 1), ..., color = color)
 }
 
 #' @rdname print.rvar
 #' @export
-type_sum.rvar <- function(x) {
-  "rvar"
+str.rvar <- function(x, ..., vec.len = NULL, width = NULL) {
+  .draws = field(x, 1)
+  width = width %||% getOption("width")
+  vec.len = vec.len %||% getOption("str")$vec.len
+
+  # flatten all the non-draws dimensions
+  .dim = dim(.draws)
+  dim(.draws) = c(prod(.dim[-length(.dim)]), .dim[length(.dim)])
+
+  if (dim(.draws)[[1]] > vec.len) {
+    .draws = .draws[1:vec.len, ]
+    ellipsis = " ..."
+  } else {
+    ellipsis = ""
+  }
+
+  cat0(" ", rvar_type_abbr(x), " " , paste(format_draws(.draws), collapse = " "), ellipsis, "\n")
+  invisible(x)
 }
+
+
+# pillar methods ----------------------------------------------------------
 
 #' @rdname print.rvar
 #' @export
@@ -49,6 +54,35 @@ pillar_shaft.rvar <- function(x, ...) {
   pillar::new_pillar_shaft_simple(out, align = "right")
 }
 
+#' @importFrom pillar is_vector_s3
+#' @export
+is_vector_s3.rvar <- function(x) TRUE
+
+
+
+# draws formatting methods ------------------------------------------------
+
+# formats a draws array for display as individual "variables" (i.e. maintaining
+# its dimensions except for the dimension representing draws)
+format_draws = function(draws, ...) UseMethod("format_draws")
+
+format_draws.default = function(draws, ...) {
+  stop("IMPLEMENT ME")
+}
+
+format_draws.numeric = function(draws, ..., color = FALSE) {
+  summary_dimensions = seq_len(length(dim(draws)) - 1)
+
+  .mean = apply(draws, summary_dimensions, mean)
+  .sd = apply(draws, summary_dimensions, sd)
+  out = format_mean_sd(.mean, .sd, color = color)
+
+  dim(out) = dim(draws)[summary_dimensions]
+  dimnames(out) = dimnames(draws)[summary_dimensions]
+  out
+}
+
+format_draws.logical = format_draws.numeric
 
 # formatting helpers ------------------------------------------------------
 
@@ -67,4 +101,13 @@ format_sd = function(x, color = FALSE) {
 
 format_mean_sd = function(.mean, .sd, color = FALSE) {
   paste0(format_mean(.mean, color = color), format_sd(.sd, color = color))
+}
+
+# succinct type summary for an rvar
+rvar_type_abbr = function(x) {
+  .draws = field(x, 1)
+  .dim = dim(.draws)
+
+  dim_str = paste0("1:", .dim[-length(.dim)], collapse = ", ")
+  paste0("rvar<", ndraws(x), "> [", dim_str, "]")
 }
