46de9c46ea9e4f398efb1d70b59310c1a8d6f02a
jgabry <jgabry@gmail.com>
2020-06-12 14:46:54 -0400

    update description

--- R/posterior-package.R
+++ R/posterior-package.R
@@ -7,14 +7,22 @@
 #' @import checkmate
 #' @import stats
 #'
-#' @description The \pkg{posterior} package is intended to provide useful tools
-#'   for both users and developers of packages for fitting Bayesian models or
-#'   working with output from Bayesian models. The primary goals of the
-#'   package are to:
-#'   * Efficiently convert between many different useful formats of
-#'     draws (samples) from posterior or prior distributions.
-#'   * Provide various summaries of draws in convenient formats.
-#'   * Provide implementations of state of the art MCMC diagnostics without
-#'     requiring a dependency on packages like \pkg{rstan}.
+#' @description
+#' \if{html}{
+#'    \figure{stanlogo.png}{options: width="50"}
+#'    \url{https://mc-stan.org/posterior}
+#' }
+#'
+#' The \pkg{posterior} package is intended to provide useful tools
+#' for both users and developers of packages for fitting Bayesian models or
+#' working with output from Bayesian models. The primary goals of the package
+#' are to:
+#' * Efficiently convert between many different useful formats of
+#' draws (samples) from posterior or prior distributions.
+#' * Provide consistent methods for operations commonly performed on draws,
+#' for example, subsetting, binding, or mutating draws.
+#' * Provide various summaries of draws in convenient formats.
+#' * Provide lightweight implementations of state of the art posterior inference
+#' diagnostics.
 #'
 NULL
