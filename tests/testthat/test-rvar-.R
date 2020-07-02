803da7f0e34d38f37810b8471e8fa0c6deccee78
Matthew Kay <matthew.kay@gmail.com>
2020-07-02 15:23:39 -0400

    make tests generic to draws dim position

--- tests/testthat/test-rvar-.R
+++ tests/testthat/test-rvar-.R
@@ -1,15 +1,19 @@
 # indexing ----------------------------------------------------------------
 
+# function for making rvars from arrays that expects last index to be
+# draws (for testing so that when array structure changes tests don't have to)
+rvar_from_array = new_rvar
+
 test_that("indexing with [[ works on a vector", {
   x_array <- array(1:20, dim = c(4,5), dimnames = list(A = paste0("a", 1:4), NULL))
-  x = new_rvar(x_array)
+  x = rvar_from_array(x_array)
 
   # [[ indexing should drop names (but not indices)
   x_array_ref = x_array
   dimnames(x_array_ref) <- NULL
 
-  expect_identical(x[[3]], new_rvar(x_array_ref[3,, drop = FALSE]))
-  expect_identical(x[["a2"]], new_rvar(x_array_ref[2,, drop = FALSE]))
+  expect_identical(x[[3]], rvar_from_array(x_array_ref[3,, drop = FALSE]))
+  expect_identical(x[["a2"]], rvar_from_array(x_array_ref[2,, drop = FALSE]))
 
   expect_error(x[[]])
   expect_error(x[[NA]])
@@ -27,33 +31,33 @@ test_that("indexing with [[ works on a vector", {
 
 test_that("indexing with [ works on a vector", {
   x_array = array(1:20, dim = c(4,5), dimnames = list(A = paste0("a", 1:4), NULL))
-  x = new_rvar(x_array)
+  x = rvar_from_array(x_array)
 
   expect_identical(x[], x)
 
-  expect_identical(x[3], new_rvar(x_array[3,, drop = FALSE]))
-  expect_identical(x["a2"], new_rvar(x_array["a2",, drop = FALSE]))
-  expect_identical(x[c(1,3)], new_rvar(x_array[c(1,3),, drop = FALSE]))
-  expect_identical(x[c("a2","a4")], new_rvar(x_array[c("a2","a4"),, drop = FALSE]))
+  expect_identical(x[3], rvar_from_array(x_array[3,, drop = FALSE]))
+  expect_identical(x["a2"], rvar_from_array(x_array["a2",, drop = FALSE]))
+  expect_identical(x[c(1,3)], rvar_from_array(x_array[c(1,3),, drop = FALSE]))
+  expect_identical(x[c("a2","a4")], rvar_from_array(x_array[c("a2","a4"),, drop = FALSE]))
 
-  expect_identical(x[c(-1,-3)], new_rvar(x_array[c(-1,-3),, drop = FALSE]))
+  expect_identical(x[c(-1,-3)], rvar_from_array(x_array[c(-1,-3),, drop = FALSE]))
 
-  expect_identical(x[TRUE], new_rvar(x_array[TRUE,, drop = FALSE]))
-  expect_identical(x[c(TRUE,FALSE)], new_rvar(x_array[c(TRUE,FALSE),, drop = FALSE]))
-  expect_identical(x[c(TRUE,FALSE,TRUE)], new_rvar(x_array[c(TRUE,FALSE,TRUE),, drop = FALSE]))
-  expect_identical(x[c(TRUE,FALSE,FALSE,TRUE)], new_rvar(x_array[c(TRUE,FALSE,FALSE,TRUE),, drop = FALSE]))
+  expect_identical(x[TRUE], rvar_from_array(x_array[TRUE,, drop = FALSE]))
+  expect_identical(x[c(TRUE,FALSE)], rvar_from_array(x_array[c(TRUE,FALSE),, drop = FALSE]))
+  expect_identical(x[c(TRUE,FALSE,TRUE)], rvar_from_array(x_array[c(TRUE,FALSE,TRUE),, drop = FALSE]))
+  expect_identical(x[c(TRUE,FALSE,FALSE,TRUE)], rvar_from_array(x_array[c(TRUE,FALSE,FALSE,TRUE),, drop = FALSE]))
 
   # dropping should preserve names (hence the drop = FALSE on x_array for this test)
-  expect_identical(x["a1", drop = TRUE], new_rvar(x_array["a1",, drop = FALSE]))
-  expect_identical(x[1:2, drop = TRUE], new_rvar(x_array[1:2,, drop = FALSE]))
+  expect_identical(x["a1", drop = TRUE], rvar_from_array(x_array["a1",, drop = FALSE]))
+  expect_identical(x[1:2, drop = TRUE], rvar_from_array(x_array[1:2,, drop = FALSE]))
 
   # indexing beyond the end of the array should result in NAs, to mimic normal vector indexing
-  expect_identical(x[c(4,5)], new_rvar(x_array[c(4,NA_integer_),, drop = FALSE]))
-  expect_identical(x[c(8,9)], new_rvar(x_array[c(NA_integer_,NA_integer_),, drop = FALSE]))
+  expect_identical(x[c(4,5)], rvar_from_array(x_array[c(4,NA_integer_),, drop = FALSE]))
+  expect_identical(x[c(8,9)], rvar_from_array(x_array[c(NA_integer_,NA_integer_),, drop = FALSE]))
 
-  expect_identical(x[NA], new_rvar(x_array[NA,, drop = FALSE]))
-  expect_identical(x[NA_integer_], new_rvar(x_array[NA_integer_,, drop = FALSE]))
-  expect_identical(x[rep(NA_integer_,7)], new_rvar(x_array[rep(NA_integer_,7),, drop = FALSE]))
+  expect_identical(x[NA], rvar_from_array(x_array[NA,, drop = FALSE]))
+  expect_identical(x[NA_integer_], rvar_from_array(x_array[NA_integer_,, drop = FALSE]))
+  expect_identical(x[rep(NA_integer_,7)], rvar_from_array(x_array[rep(NA_integer_,7),, drop = FALSE]))
 
   expect_identical(x[NULL], new_rvar())
 
@@ -65,34 +69,34 @@ test_that("indexing with [ works on a matrix", {
     1:24, dim = c(4,3,2),
     dimnames = list(A = paste0("a", 1:4), B = paste0("b", 1:3))
   )
-  x = new_rvar(x_array)
+  x = rvar_from_array(x_array)
 
   expect_identical(x[], x)
 
-  expect_identical(x[2], new_rvar(x_array[2,,, drop = FALSE]))
-  expect_identical(x["a2"], new_rvar(x_array["a2",,, drop = FALSE]))
-  expect_identical(x[c(1,2)], new_rvar(x_array[c(1,2),,, drop = FALSE]))
-  expect_identical(x[,c(1,3)], new_rvar(x_array[,c(1,3),, drop = FALSE]))
-  expect_identical(x[,c("b2","b3")], new_rvar(x_array[,c("b2","b3"),, drop = FALSE]))
+  expect_identical(x[2], rvar_from_array(x_array[2,,, drop = FALSE]))
+  expect_identical(x["a2"], rvar_from_array(x_array["a2",,, drop = FALSE]))
+  expect_identical(x[c(1,2)], rvar_from_array(x_array[c(1,2),,, drop = FALSE]))
+  expect_identical(x[,c(1,3)], rvar_from_array(x_array[,c(1,3),, drop = FALSE]))
+  expect_identical(x[,c("b2","b3")], rvar_from_array(x_array[,c("b2","b3"),, drop = FALSE]))
 
-  expect_identical(x[,c(-1,-3)], new_rvar(x_array[,c(-1,-3),, drop = FALSE]))
+  expect_identical(x[,c(-1,-3)], rvar_from_array(x_array[,c(-1,-3),, drop = FALSE]))
 
-  expect_identical(x[TRUE], new_rvar(x_array[TRUE,,, drop = FALSE]))
-  expect_identical(x[c(TRUE,FALSE)], new_rvar(x_array[c(TRUE,FALSE),,, drop = FALSE]))
-  expect_identical(x[c(TRUE,FALSE,TRUE)], new_rvar(x_array[c(TRUE,FALSE,TRUE),,, drop = FALSE]))
-  expect_identical(x[c(TRUE,FALSE,FALSE,TRUE)], new_rvar(x_array[c(TRUE,FALSE,FALSE,TRUE),,, drop = FALSE]))
+  expect_identical(x[TRUE], rvar_from_array(x_array[TRUE,,, drop = FALSE]))
+  expect_identical(x[c(TRUE,FALSE)], rvar_from_array(x_array[c(TRUE,FALSE),,, drop = FALSE]))
+  expect_identical(x[c(TRUE,FALSE,TRUE)], rvar_from_array(x_array[c(TRUE,FALSE,TRUE),,, drop = FALSE]))
+  expect_identical(x[c(TRUE,FALSE,FALSE,TRUE)], rvar_from_array(x_array[c(TRUE,FALSE,FALSE,TRUE),,, drop = FALSE]))
 
   # dropping works
-  expect_identical(x["a1", drop = TRUE], new_rvar(drop(x_array["a1",,, drop = TRUE])))
-  expect_identical(x[1:2, drop = TRUE], new_rvar(x_array[1:2,,, drop = TRUE]))
+  expect_identical(x["a1", drop = TRUE], rvar_from_array(drop(x_array["a1",,, drop = TRUE])))
+  expect_identical(x[1:2, drop = TRUE], rvar_from_array(x_array[1:2,,, drop = TRUE]))
 
   # indexing beyond the end of the array should result in NAs, to mimic normal vector indexing
-  expect_identical(x[c(4,5)], new_rvar(x_array[c(4,NA_integer_),,, drop = FALSE]))
-  expect_identical(x[c(8,9)], new_rvar(x_array[c(NA_integer_,NA_integer_),,, drop = FALSE]))
+  expect_identical(x[c(4,5)], rvar_from_array(x_array[c(4,NA_integer_),,, drop = FALSE]))
+  expect_identical(x[c(8,9)], rvar_from_array(x_array[c(NA_integer_,NA_integer_),,, drop = FALSE]))
 
-  expect_identical(x[NA], new_rvar(x_array[NA,,, drop = FALSE]))
-  expect_identical(x[NA_integer_], new_rvar(x_array[NA_integer_,,, drop = FALSE]))
-  expect_identical(x[rep(NA_integer_,7)], new_rvar(x_array[rep(NA_integer_,7),,, drop = FALSE]))
+  expect_identical(x[NA], rvar_from_array(x_array[NA,,, drop = FALSE]))
+  expect_identical(x[NA_integer_], rvar_from_array(x_array[NA_integer_,,, drop = FALSE]))
+  expect_identical(x[rep(NA_integer_,7)], rvar_from_array(x_array[rep(NA_integer_,7),,, drop = FALSE]))
 
   expect_identical(x[NULL], new_rvar())
 
@@ -119,25 +123,25 @@ test_that("rvars work in tibbles", {
   skip_if_not_installed("tidyr")
 
   x_array = array(1:20, dim = c(4,5))
-  x = new_rvar(x_array)
+  x = rvar_from_array(x_array)
   df = tibble::tibble(x, y = x + 1)
 
   expect_identical(df$x, x)
-  expect_identical(df$y, new_rvar(x_array + 1))
+  expect_identical(df$y, rvar_from_array(x_array + 1))
   expect_identical(dplyr::mutate(df, z = x)$z, x)
 
-  expect_equal(dplyr::mutate(df, z = x * 2)$z, new_rvar(x_array * 2))
+  expect_equal(dplyr::mutate(df, z = x * 2)$z, rvar_from_array(x_array * 2))
   expect_equal(
     dplyr::mutate(dplyr::group_by(df, 1:4), z = x * 2)$z,
-    new_rvar(x_array * 2)
+    rvar_from_array(x_array * 2)
   )
 
   df = tibble::tibble(g = letters[1:4], x)
   ref = tibble::tibble(
-    a = new_rvar(x_array[1,, drop = FALSE]),
-    b = new_rvar(x_array[2,, drop = FALSE]),
-    c = new_rvar(x_array[3,, drop = FALSE]),
-    d = new_rvar(x_array[4,, drop = FALSE])
+    a = rvar_from_array(x_array[1,, drop = FALSE]),
+    b = rvar_from_array(x_array[2,, drop = FALSE]),
+    c = rvar_from_array(x_array[3,, drop = FALSE]),
+    d = rvar_from_array(x_array[4,, drop = FALSE])
   )
   expect_equal(tidyr::pivot_wider(df, names_from = g, values_from = x), ref)
   expect_equal(tidyr::pivot_longer(ref, a:d, names_to = "g", values_to = "x"), df)
--- tests/testthat/test-rvar-math.R
+++ tests/testthat/test-rvar-math.R
@@ -1,28 +1,32 @@
+# function for making rvars from arrays that expects last index to be
+# draws (for testing so that when array structure changes tests don't have to)
+rvar_from_array = new_rvar
+
 test_that("arithmetic works", {
   x_array = array(1:24, dim = c(2,3,4))
-  x = new_rvar(x_array)
+  x = rvar_from_array(x_array)
   y_array = array(c(2:13,12:1), dim = c(2,3,4))
-  y = new_rvar(y_array)
+  y = rvar_from_array(y_array)
 
-  expect_identical(x + 2, new_rvar(x_array + 2))
-  expect_identical(2 + x, new_rvar(x_array + 2))
-  expect_identical(x + y, new_rvar(x_array + y_array))
+  expect_identical(x + 2, rvar_from_array(x_array + 2))
+  expect_identical(2 + x, rvar_from_array(x_array + 2))
+  expect_identical(x + y, rvar_from_array(x_array + y_array))
 
-  expect_identical(x - 2, new_rvar(x_array - 2))
-  expect_identical(2 - x, new_rvar(2 - x_array))
-  expect_identical(x - y, new_rvar(x_array - y_array))
+  expect_identical(x - 2, rvar_from_array(x_array - 2))
+  expect_identical(2 - x, rvar_from_array(2 - x_array))
+  expect_identical(x - y, rvar_from_array(x_array - y_array))
 
-  expect_identical(x * 2, new_rvar(x_array * 2))
-  expect_identical(2 * x, new_rvar(x_array * 2))
-  expect_identical(x * y, new_rvar(x_array * y_array))
+  expect_identical(x * 2, rvar_from_array(x_array * 2))
+  expect_identical(2 * x, rvar_from_array(x_array * 2))
+  expect_identical(x * y, rvar_from_array(x_array * y_array))
 
-  expect_identical(x / 2, new_rvar(x_array / 2))
-  expect_identical(2 / x, new_rvar(2 / x_array))
-  expect_identical(x / y, new_rvar(x_array / y_array))
+  expect_identical(x / 2, rvar_from_array(x_array / 2))
+  expect_identical(2 / x, rvar_from_array(2 / x_array))
+  expect_identical(x / y, rvar_from_array(x_array / y_array))
 
-  expect_identical(x ^ 2, new_rvar((x_array) ^ 2))
-  expect_identical(2 ^ x, new_rvar(2 ^ (x_array)))
-  expect_identical(x ^ y, new_rvar(x_array ^ y_array))
+  expect_identical(x ^ 2, rvar_from_array((x_array) ^ 2))
+  expect_identical(2 ^ x, rvar_from_array(2 ^ (x_array)))
+  expect_identical(x ^ y, rvar_from_array(x_array ^ y_array))
 })
 
 test_that("logical operators work", {
@@ -42,42 +46,42 @@ test_that("logical operators work", {
 
 test_that("comparison operators work", {
   x_array = array(1:24, dim = c(2,3,4))
-  x = new_rvar(x_array)
+  x = rvar_from_array(x_array)
   y_array = array(c(2:13,12:1), dim = c(2,3,4))
-  y = new_rvar(y_array)
+  y = rvar_from_array(y_array)
 
-  expect_identical(x < 5, new_rvar(x_array < 5))
-  expect_identical(5 < x, new_rvar(5 < x_array))
-  expect_identical(x < y, new_rvar(x_array < y_array))
+  expect_identical(x < 5, rvar_from_array(x_array < 5))
+  expect_identical(5 < x, rvar_from_array(5 < x_array))
+  expect_identical(x < y, rvar_from_array(x_array < y_array))
 
-  expect_identical(x <= 5, new_rvar(x_array <= 5))
-  expect_identical(5 <= x, new_rvar(5 <= x_array))
-  expect_identical(x <= y, new_rvar(x_array <= y_array))
+  expect_identical(x <= 5, rvar_from_array(x_array <= 5))
+  expect_identical(5 <= x, rvar_from_array(5 <= x_array))
+  expect_identical(x <= y, rvar_from_array(x_array <= y_array))
 
-  expect_identical(x > 5, new_rvar(x_array > 5))
-  expect_identical(5 > x, new_rvar(5 > x_array))
-  expect_identical(x > y, new_rvar(x_array > y_array))
+  expect_identical(x > 5, rvar_from_array(x_array > 5))
+  expect_identical(5 > x, rvar_from_array(5 > x_array))
+  expect_identical(x > y, rvar_from_array(x_array > y_array))
 
-  expect_identical(x >= 5, new_rvar(x_array >= 5))
-  expect_identical(5 >= x, new_rvar(5 >= x_array))
-  expect_identical(x >= y, new_rvar(x_array >= y_array))
+  expect_identical(x >= 5, rvar_from_array(x_array >= 5))
+  expect_identical(5 >= x, rvar_from_array(5 >= x_array))
+  expect_identical(x >= y, rvar_from_array(x_array >= y_array))
 
-  expect_identical(x == 5, new_rvar(x_array == 5))
-  expect_identical(5 == x, new_rvar(5 == x_array))
-  expect_identical(x == y, new_rvar(x_array == y_array))
+  expect_identical(x == 5, rvar_from_array(x_array == 5))
+  expect_identical(5 == x, rvar_from_array(5 == x_array))
+  expect_identical(x == y, rvar_from_array(x_array == y_array))
 
-  expect_identical(x != 5, new_rvar(x_array != 5))
-  expect_identical(5 != x, new_rvar(5 != x_array))
-  expect_identical(x != y, new_rvar(x_array != y_array))
+  expect_identical(x != 5, rvar_from_array(x_array != 5))
+  expect_identical(5 != x, rvar_from_array(5 != x_array))
+  expect_identical(x != y, rvar_from_array(x_array != y_array))
 })
 
 test_that("matrix multiplication works", {
   x_array = array(1:24, dim = c(2,3,4))
-  x = new_rvar(x_array)
+  x = rvar_from_array(x_array)
   y_array = array(c(2:13,12:1), dim = c(3,2,4))
-  y = new_rvar(y_array)
+  y = rvar_from_array(y_array)
 
-  xy_ref = new_rvar(abind::abind(rev.along = 0,
+  xy_ref = rvar_from_array(abind::abind(rev.along = 0,
     x_array[,,1] %*% y_array[,,1],
     x_array[,,2] %*% y_array[,,2],
     x_array[,,3] %*% y_array[,,3],
@@ -87,11 +91,11 @@ test_that("matrix multiplication works", {
 
 
   x_array = array(1:6, dim = c(3,2))
-  x = new_rvar(x_array)
+  x = rvar_from_array(x_array)
   y_array = array(7:12, dim = c(3,2))
-  y = new_rvar(y_array)
+  y = rvar_from_array(y_array)
 
-  xy_ref = new_rvar(abind::abind(rev.along = 0,
+  xy_ref = rvar_from_array(abind::abind(rev.along = 0,
     x_array[,1] %*% y_array[,1],
     x_array[,2] %*% y_array[,2]
   ))
@@ -99,7 +103,7 @@ test_that("matrix multiplication works", {
 
 
   x_array = array(1:24, dim = c(1,2,3,4))
-  x = new_rvar(x_array)
+  x = rvar_from_array(x_array)
 
   expect_error(x %*% 1, "not a vector or matrix")
   expect_error(1 %*% x, "not a vector or matrix")
@@ -110,11 +114,11 @@ test_that("matrix multiplication works", {
 
 test_that("matrix transpose works", {
   x_array = array(1:24, dim = c(2,3,4))
-  x = new_rvar(x_array)
-  x_t = new_rvar(aperm(x_array, c(2,1,3)))
+  x = rvar_from_array(x_array)
+  x_t = rvar_from_array(aperm(x_array, c(2,1,3)))
 
   expect_error(t(rvar()))
   expect_equal(t(x), x_t)
-  expect_equal(t(new_rvar(array(1:5, c(10,1)))), new_rvar(array(1:5, c(1,10,1))))
+  expect_equal(t(rvar_from_array(array(1:5, c(10,1)))), rvar_from_array(array(1:5, c(1,10,1))))
 })
 
