0184c56f0f6f7a7f9183f418983c1d028fef2942
Paul Buerkner <paul.buerkner@gmail.com>
2021-03-26 17:43:40 +0100

    fix doc typo

--- man-roxygen/args-rvar-dimnames.R
+++ man-roxygen/args-rvar-dimnames.R
@@ -1,2 +1,2 @@
 #' @param dimnames Override for the names of the dimensions of the [`rvar`] to be created (see [dimnames()]): a list of character
-#' vectors giving names of the maximal indices in each dimension. If `NULL` (the default), this is determined by the input.
+#' vectors giving the names in each dimension. If `NULL` (the default), this is determined by the input.
