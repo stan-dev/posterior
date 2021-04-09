99c06775a81cf33c8dba1232431da30adf78ef34
Matthew Kay <matthew.kay@gmail.com>
2021-04-08 20:17:25 -0500

    working and tested rvar_apply, for #117

--- R/rvar-apply.R
+++ R/rvar-apply.R
@@ -10,8 +10,9 @@
 #' and columns. If `.x` has named dimensions, this can be a character vector selecting
 #' dimension names.
 #' @param .f the function to be applied. This must be a function that returns an [`rvar`].
-#' The dimensions of the result of `.f` applied to each margin of `.x` must be the same as
-#' each other, otherwise the resulting [`rvar`] cannot be simplified. See 'Details'.
+#' The dimensions of the result of `.f` applied to each margin of `.x` must be able to be
+#' broadasted to a common shape, otherwise the resulting [`rvar`] cannot be simplified.
+#' See 'Details'.
 #' @param ... optional arguments to `.f`.
 #'
 #' @details
@@ -19,7 +20,9 @@
 #' This function acts much like `apply()`, except that the function passed to it (`.f`)
 #' must return [`rvar`]s, and the result is simplified into an [`rvar`]. Unlike
 #' `apply()`, it also keeps the dimensions of the returned values along each margin,
-#' rather than simplifying each margin to a vector.
+#' rather than simplifying each margin to a vector, and if the results of `.f` do
+#' not all have the same dimensions, it applies the [`rvar`] broadcasting rules to
+#' bind results together rather than using vector recycling.
 #'
 #' If you wish to apply functions over [`rvar`]s where the result is not intended to
 #' be simplified into an [`rvar`], you can use the standard `apply()`, `lapply()`,
@@ -30,13 +33,11 @@
 #'
 #' @return An [`rvar`].
 #'
-#' If each call to `.f` returns an [`rvar`] of dimension `d`, then `rvar_apply()`
-#' returns an [`rvar`] of dimension `c(d, dim(.x)[.margin])` if `d != 1`.
-#' If `d` equals `1`, `rvar_apply()` returns an [`rvar`] of dimension `dim(.x)[.margin]`
-#' otherwise. If `d` is `0`, the result has length `0` but not necessarily the 'correct' dimension.
-#'
-#' The calls to `.f` must return [`rvar`]s of the same shape; otherwise `rvar_apply()`
-#' returns an error.
+#' If the result of each call to `.f` returns an [`rvar`] of dimension `d` after
+#' being broadcast to a common shape, then `rvar_apply()` returns an [`rvar`] of
+#' dimension `c(d, dim(.x)[.margin])`. If the last dimension of the result would
+#' be `1`, it is dropped (other dimensions equal to `1` are retained). If `d` is
+#' `0`, the result has length `0` but not necessarily the 'correct' dimension.
 #'
 #' @examples
 #'
@@ -52,34 +53,37 @@
 #' @export
 rvar_apply <- function(.x, .margin, .f, ...) {
   # this should return a list of rvars
-  out <- apply(.x, .margin, .f, ...)
-  if (!is.list(out) || !is_rvar(out[[1]])) {
+  rvar_list <- apply(.x, .margin, .f, ...)
+  if (!is.list(rvar_list) || !all(sapply(rvar_list, is_rvar))) {
     stop2("The function passed to rvar_apply() must return rvars.")
   }
 
   # get the dimensions of the results in each cell
-  check_same_fun_output(out, dim)
-  cell_dim <- dim(out[[1]])
-
+  cell_dim <- dim(rvar_list[[1]])
   # get the dimensions from the original array that are being kept
-  original_dim <- dim(out) %||% length(out)
+  marginal_dim <- dim(rvar_list) %||% length(rvar_list)
 
-  # bind the output into an rvar and move the original dimensions (which are
-  # currently flattened into a single dimension at the end) to the front
-  # (this could probably be more efficient)
-  out <- bind_rvars(out, rep("", length(out)), axis = length(cell_dim) + 1)
-  n_dim <- length(dim(out))
-  out <- aperm(out, c(n_dim, seq_len(n_dim - 1)))
-
-  # restore the shape of the original dimensions
-  if (isTRUE(cell_dim == 1)) {
-    # if the dimensions of the value in each cell are 1, we won't add it as
-    # a new dimension
-    dim(out) <- original_dim
-  } else {
-    dim(out) <- c(original_dim, cell_dim)
+  # bind the output into an rvar along the first dimension (which the
+  # marginal dimensions flattened into a single dim)
+  out <- rvar_list[[1]]
+  dim(out) <- c(1, dim(out))
+  # process remaining rvars in succession, binding them to the output
+  for (i in seq_along(rvar_list)[-1]) {
+    rvar_i <- rvar_list[[i]]
+    dim(rvar_i) <- c(1, dim(rvar_i))
+    out <- broadcast_and_bind_rvars(out, rvar_i, 1)
   }
-  dimnames(out)[seq_along(original_dim)] <- dimnames(.x)[.margin]
+
+  # restore the shape of the marginal dimensions
+  dim(out) <- c(marginal_dim, dim(out)[-1])
+  # if the last dimension is 1, drop it
+  n_dim <- length(dim(out))
+  if (dim(out)[[n_dim]] == 1) {
+    dim(out) <- dim(out)[-n_dim]
+  }
+  # restore marginal dimnames
+  marginal_dim_i <- seq_along(marginal_dim)
+  out <- copy_dimnames(.x, .margin, out, marginal_dim_i)
 
   out
 }
