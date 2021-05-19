9669b9da975bfed39e2815f656f45947cda7e746
Jacob Socolar <jacob.socolar@gmail.com>
2021-05-18 23:13:00 -0500

    still working on passing function definitions to the cluster on windows part 15

--- R/summarise_draws.R
+++ R/summarise_draws.R
@@ -210,12 +210,39 @@ summarise_draws.draws <- function(x, ..., .args = list(), cores = 1) {
       on.exit(parallel::stopCluster(cl))
       parallel::clusterExport(cl = cl, 
                               c(unclass(lsf.str(envir = asNamespace("posterior"), all = T)),
-                                unclass(lsf.str(envir = asNamespace("checkmate"), all = T))))
-      parallel::clusterEvalQ(cl, 
+                                unclass(lsf.str(envir = asNamespace("checkmate"), all = T)),
+                                unclass(lsf.str(envir = asNamespace("rlang"), all = T))))
+      parallel::clusterEvalQ(cl,
                              "%and%" <- function(lhs, rhs) {
-                                          if (isTRUE(lhs)) rhs else lhs
-                                        }
-                             )
+                               if (isTRUE(lhs)) rhs else lhs
+                             }
+                             `%<-%` <- function(lhs, value) {
+                               lhs <- substitute(lhs)
+                               env <- caller_env()
+                               
+                               if (!is_call(lhs, "c")) {
+                                 abort("The left-hand side of `%<-%` must be a call to `c()`.")
+                               }
+                               
+                               vars <- as.list(lhs[-1])
+                               
+                               if (length(value) != length(vars)) {
+                                 abort("The left- and right-hand sides of `%<-%` must be the same length.")
+                               }
+                               
+                               for (i in seq_along(vars)) {
+                                 var <- vars[[i]]
+                                 if (!is_symbol(var)) {
+                                   abort(paste0("Element ", i, " of the left-hand side of `%<-%` must be a symbol."))
+                                 }
+                                 
+                                 env[[as_string(var)]] <- value[[i]]
+                               }
+                               
+                               invisible(value)
+                             }
+      )
+      
       summary_list <- parallel::parLapply(cl = cl, X = chunk_list, fun = summarise_draws_helper2)
     } else {
       summary_list <- parallel::mclapply(X = chunk_list, FUN = summarise_draws_helper2, mc.cores = cores)
