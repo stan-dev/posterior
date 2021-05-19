d2b92298851ca5974f2c49121ce0e44d3941c723
Jacob Socolar <jacob.socolar@gmail.com>
2021-05-18 23:58:49 -0500

    still working on passing function definitions to the cluster on windows part 17

--- R/summarise_draws.R
+++ R/summarise_draws.R
@@ -209,41 +209,10 @@ summarise_draws.draws <- function(x, ..., .args = list(), cores = 1) {
       cl <- parallel::makePSOCKcluster(cores)
       on.exit(parallel::stopCluster(cl))
       parallel::clusterExport(cl = cl, 
-                              c(unclass(lsf.str(envir = asNamespace("posterior"), all = T)),
-                                unclass(lsf.str(envir = asNamespace("checkmate"), all = T)),
-                                unclass(lsf.str(envir = asNamespace("rlang"), all = T))))
-      parallel::clusterEvalQ(cl,
-                             "%and%" <- function(lhs, rhs) {
-                               if (isTRUE(lhs)) rhs else lhs
-                             }
-      )
-      parallel::clusterEvalQ(cl,
-                             `%<-%` <- function(lhs, value) {
-                               lhs <- substitute(lhs)
-                               env <- caller_env()
-                               
-                               if (!is_call(lhs, "c")) {
-                                 abort("The left-hand side of `%<-%` must be a call to `c()`.")
-                               }
-                               
-                               vars <- as.list(lhs[-1])
-                               
-                               if (length(value) != length(vars)) {
-                                 abort("The left- and right-hand sides of `%<-%` must be the same length.")
-                               }
-                               
-                               for (i in seq_along(vars)) {
-                                 var <- vars[[i]]
-                                 if (!is_symbol(var)) {
-                                   abort(paste0("Element ", i, " of the left-hand side of `%<-%` must be a symbol."))
-                                 }
-                                 
-                                 env[[as_string(var)]] <- value[[i]]
-                               }
-                               
-                               invisible(value)
-                             }
-      )
+                              unclass(lsf.str(envir = asNamespace("posterior"), all = T))
+                              )
+      parallel::clusterEvalQ(cl, library(checkmate))
+      parallel::clusterEvalQ(cl, library(rlang))
       
       summary_list <- parallel::parLapply(cl = cl, X = chunk_list, fun = summarise_draws_helper2)
     } else {
