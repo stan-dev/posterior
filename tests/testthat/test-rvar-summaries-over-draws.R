69d2b5f4ae971fd51830ab805c623649404e43b0
Matthew Kay <matthew.kay@gmail.com>
2021-08-25 11:36:53 -0500

    ensure rvar summary tests check dimnames

--- tests/testthat/test-rvar-summaries-over-draws.R
+++ tests/testthat/test-rvar-summaries-over-draws.R
@@ -1,27 +1,27 @@
 # numeric summaries -------------------------------------------------------
 
 test_that("numeric summaries work", {
-  x_array <- array(1:24, dim = c(4,2,3))
+  x_array <- array(1:24, dim = c(4,2,3), dimnames = list(NULL, a = c("a1", "a2"), b = c("b1", "b2", "b3")))
   x <- new_rvar(x_array)
 
-  expect_equal(median(x), apply(x_array, c(2,3), median), check.attributes = FALSE)
-  expect_equal(sum(x), apply(x_array, c(2,3), sum), check.attributes = FALSE)
-  expect_equal(prod(x), apply(x_array, c(2,3), prod), check.attributes = FALSE)
-  expect_equal(min(x), apply(x_array, c(2,3), min), check.attributes = FALSE)
-  expect_equal(max(x), apply(x_array, c(2,3), max), check.attributes = FALSE)
+  expect_equal(median(x), apply(x_array, c(2,3), median))
+  expect_equal(sum(x), apply(x_array, c(2,3), sum))
+  expect_equal(prod(x), apply(x_array, c(2,3), prod))
+  expect_equal(min(x), apply(x_array, c(2,3), min))
+  expect_equal(max(x), apply(x_array, c(2,3), max))
 })
 
 
 # mean --------------------------------------------------------------------
 
 test_that("means work", {
-  x_array <- array(1:24, dim = c(4,2,3))
+  x_array <- array(1:24, dim = c(4,2,3), dimnames = list(NULL, a = c("a1", "a2"), b = c("b1", "b2", "b3")))
   x <- new_rvar(x_array)
 
-  expect_equal(Pr(x < 2), apply(x_array < 2, c(2,3), mean), check.attributes = FALSE)
+  expect_equal(Pr(x < 2), apply(x_array < 2, c(2,3), mean))
   expect_error(Pr(x))
-  expect_equal(E(x), apply(x_array, c(2,3), mean), check.attributes = FALSE)
-  expect_equal(mean(x), apply(x_array, c(2,3), mean), check.attributes = FALSE)
+  expect_equal(E(x), apply(x_array, c(2,3), mean))
+  expect_equal(mean(x), apply(x_array, c(2,3), mean))
   # E() and Pr() should also work on base arrays
   expect_equal(Pr(x_array < 2), mean(x_array < 2))
   expect_error(Pr(x_array))
@@ -32,13 +32,13 @@ test_that("means work", {
 # spread ------------------------------------------------------------------
 
 test_that("spread functions work", {
-  x_array <- array(1:24, dim = c(4,2,3))
+  x_array <- array(1:24, dim = c(4,2,3), dimnames = list(NULL, a = c("a1", "a2"), b = c("b1", "b2", "b3")))
   x <- new_rvar(x_array)
 
-  expect_equal(sd(x), apply(x_array, c(2,3), sd), check.attributes = FALSE)
-  expect_equal(variance(x), apply(x_array, c(2,3), var), check.attributes = FALSE)
-  expect_equal(var(x), apply(x_array, c(2,3), var), check.attributes = FALSE)
-  expect_equal(mad(x), apply(x_array, c(2,3), mad), check.attributes = FALSE)
+  expect_equal(sd(x), apply(x_array, c(2,3), sd))
+  expect_equal(variance(x), apply(x_array, c(2,3), var))
+  expect_equal(var(x), apply(x_array, c(2,3), var))
+  expect_equal(mad(x), apply(x_array, c(2,3), mad))
 })
 
 
@@ -55,31 +55,34 @@ test_that("range works", {
 # logical summaries -------------------------------------------------------
 
 test_that("logical summaries work", {
-  x_array <- array(1:24, dim = c(4,2,3))
+  x_array <- array(1:24, dim = c(4,2,3), dimnames = list(NULL, a = c("a1", "a2"), b = c("b1", "b2", "b3")))
   x <- new_rvar(x_array)
 
-  expect_equal(all(x > 10), apply(x_array > 10, c(2,3), all), check.attributes = FALSE)
-  expect_equal(any(x > 10), apply(x_array > 10, c(2,3), any), check.attributes = FALSE)
+  expect_equal(all(x > 10), apply(x_array > 10, c(2,3), all))
+  expect_equal(any(x > 10), apply(x_array > 10, c(2,3), any))
 })
 
 
 # special value predicates ------------------------------------------------
 
 test_that("special value predicates work", {
-  x_array <- array(c(1,NA,3:4, 5:6,Inf,8, 9,-Inf,11:12, NaN,14:24), dim = c(4,2,3))
+  x_array <- array(c(1,NA,3:4, 5:6,Inf,8, 9,-Inf,11:12, NaN,14:24),
+    dim = c(4,2,3), dimnames = list(NULL, a = c("a1", "a2"), b = c("b1", "b2", "b3"))
+  )
   x <- new_rvar(x_array)
 
-  expect_equal(is.finite(x), array(c(rep(FALSE, 4), rep(TRUE, 2)), dim = c(2,3)), check.attributes = FALSE)
-  expect_equal(is.infinite(x), array(c(FALSE, TRUE, TRUE, FALSE, FALSE, FALSE), dim = c(2,3)), check.attributes = FALSE)
-  expect_equal(is.nan(x), array(c(FALSE, FALSE, FALSE, TRUE, FALSE, FALSE), dim = c(2,3)), check.attributes = FALSE)
-  expect_equal(is.na(x), array(c(TRUE, FALSE, FALSE, TRUE, FALSE, FALSE), dim = c(2,3)), check.attributes = FALSE)
+  .dimnames = list(a = c("a1", "a2"), b = c("b1", "b2", "b3"))
+  expect_equal(is.finite(x), array(c(rep(FALSE, 4), rep(TRUE, 2)), dim = c(2,3), dimnames = .dimnames))
+  expect_equal(is.infinite(x), array(c(FALSE, TRUE, TRUE, FALSE, FALSE, FALSE), dim = c(2,3), dimnames = .dimnames))
+  expect_equal(is.nan(x), array(c(FALSE, FALSE, FALSE, TRUE, FALSE, FALSE), dim = c(2,3), dimnames = .dimnames))
+  expect_equal(is.na(x), array(c(TRUE, FALSE, FALSE, TRUE, FALSE, FALSE), dim = c(2,3), dimnames = .dimnames))
 })
 
 
 # anyNA -------------------------------------------------------------------
 
 test_that("anyNA works", {
-  x_array <- array(1:24, dim = c(4,2,3))
+  x_array <- array(1:24, dim = c(4,2,3), dimnames = list(NULL, a = c("a1", "a2"), b = c("b1", "b2", "b3")))
   x <- new_rvar(x_array)
 
   expect_equal(anyNA(x), FALSE)
