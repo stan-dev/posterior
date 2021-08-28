2ade4c7a1bf9df1836b81798eae34c47b69b5960
Matthew Kay <matthew.kay@gmail.com>
2021-08-27 19:23:18 -0500

    fix some vector recycling issues with rvar_rng, closes #195

--- tests/testthat/test-rvar-rfun.R
+++ tests/testthat/test-rvar-rfun.R
@@ -38,3 +38,30 @@ test_that("rvar_rng works", {
   expect_equal(mean(x), 1:10, tolerance = 0.1)
   expect_equal(apply(draws_of(x), 2, sd), rep(1.7, 10), tolerance = 0.1)
 })
+
+test_that("rvar_rng recycling works with numeric and rvar arguments", {
+  # a fake random number generator that we can use to ensure draws are being
+  # correctly reshaped and lined up
+  rfake = function(n, tens, ones) {
+    rep_len(tens * 10 + ones, length.out = n)
+  }
+
+  # numeric arguments
+  ref <- rvar(array(c(11, 11, 22, 22, 13, 13, 24, 24), dim = c(2,4)))
+  expect_equal(rvar_rng(rfake, 4, 1:2, 1:4, ndraws = 2), ref)
+
+  # mixed numeric and rvar arguments
+  ones <- rvar(array(1:8, dim = c(2,4)))
+  ref <- rvar(array(c(11, 12, 23, 24, 15, 16, 27, 28), dim = c(2,4)))
+  expect_equal(rvar_rng(rfake, 4, 1:2, ones), ref)
+
+  # rvar arguments
+  x <- c(15, 26, 37, 48)
+  expect_equal(rvar_rng(rfake, 1, rvar(1:4), rvar(5:8)), rvar(x))
+  expect_equal(rvar_rng(rfake, 2, rvar(1:4), rvar(5:8)), rvar(array(c(x, x), dim = c(4, 2))))
+
+  tens <- rvar(array(1:4, dim = c(2,2)))
+  ones <- rvar(array(1:8, dim = c(2,4)))
+  ref <- rvar(array(c(11, 22, 33, 44, 15, 26, 37, 48), dim = c(2,4)))
+  expect_equal(rvar_rng(rfake, 4, tens, ones), ref)
+})
