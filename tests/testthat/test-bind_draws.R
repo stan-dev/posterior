e861f34cf079c9a1a2bc6fd8d6b04bd4c76caa03
helske <jouni.helske@iki.fi>
2021-09-16 07:56:52 +0300

    add more tests bind_draws

--- tests/testthat/test-bind_draws.R
+++ tests/testthat/test-bind_draws.R
@@ -98,6 +98,39 @@ test_that("bind_draws works for draws_df objects", {
   expect_equal(draws_new, draws1)
 })
 
+
+test_that("bind_draws works for multiple draws_df objects", {
+
+  draws1 <- as_draws_df(example_draws())
+  draws2 <- subset_draws(draws1, chain = 2)
+  draws3 <-  subset_draws(draws1, chain = 3)
+
+  draws12 <- bind_draws(draws1, draws2, along = "chain")
+  draws123 <- bind_draws(draws12, draws3, along = "chain")
+  draws_all <- bind_draws(draws1, draws2, draws3, along = "chain")
+  expect_equal(draws123, draws_all)
+  expect_equal(nchains(draws_all),
+    nchains(draws1) + nchains(draws2) + nchains(draws3))
+  expect_equal(ndraws(draws_all),
+    ndraws(draws1) + ndraws(draws2) + ndraws(draws3))
+  expect_equal(niterations(draws_all), niterations(draws1))
+  expect_equal(variables(draws_all), variables(draws1))
+
+  draws4 <-  subset_draws(draws1, chain = 4)
+  draws23 <- bind_draws(draws2, draws3, along = "iteration")
+  draws234 <- bind_draws(draws23, draws4, along = "iteration")
+  draws_all <- bind_draws(draws2, draws3, draws4, along = "iteration")
+  expect_equal(draws234, draws_all)
+
+  expect_equal(nchains(draws_all), 1L)
+  expect_equal(niterations(draws_all),
+    niterations(draws2) + niterations(draws3) + niterations(draws4))
+  expect_equal(niterations(draws_all), ndraws(draws_all))
+
+  expect_equal(variables(draws_all), variables(draws2))
+
+})
+
 test_that("bind_draws works for draws_list objects", {
   draws1 <- as_draws_list(example_draws())
   draws2 <- subset_draws(draws1, chain = 2)
