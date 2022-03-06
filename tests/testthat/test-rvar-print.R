21be226d20df7ca38cc9a93e07a4ea86150338c4
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2022-03-06 20:55:00 +0100

    fix tests for CRAN

--- tests/testthat/test-rvar-print.R
+++ tests/testthat/test-rvar-print.R
@@ -1,51 +1,48 @@
 test_that("basic print.rvar works", {
-  skip_on_os("windows")
   x <- rvar(array(1:12, dim = c(2,2,3)))
   x_with_chains <- rvar(array(1:12, dim = c(2,2,3)), nchains = 2)
 
-  expect_output(print(rvar(), color = FALSE),
-"rvar<1>[0] mean ± sd:
-NULL",
-    fixed = TRUE
+  expect_output(
+    print(rvar(), color = FALSE),
+    "rvar<1>\\[0\\] mean . sd:\nNULL"
   )
 
-  expect_output(print(x, color = FALSE),
-"rvar<2>[2,3] mean ± sd:\
-     [,1]         [,2]         [,3]        \
-[1,]  1.5 ± 0.71   5.5 ± 0.71   9.5 ± 0.71 \
-[2,]  3.5 ± 0.71   7.5 ± 0.71  11.5 ± 0.71 ",
-    fixed = TRUE
+  expect_output(
+    print(x, color = FALSE),
+    paste0(
+      "rvar<2>\\[2,3\\] mean . sd:\n",
+      "     \\[,1\\]         \\[,2\\]         \\[,3\\]        \\n",
+      "\\[1,\\]  1.5 . 0.71   5.5 . 0.71   9.5 . 0.71 \n",
+      "\\[2,\\]  3.5 . 0.71   7.5 . 0.71  11.5 . 0.71"
+    )
   )
 
-  expect_output(print(x_with_chains, color = FALSE),
-"rvar<1,2>[2,3] mean ± sd:\
-     [,1]         [,2]         [,3]        \
-[1,]  1.5 ± 0.71   5.5 ± 0.71   9.5 ± 0.71 \
-[2,]  3.5 ± 0.71   7.5 ± 0.71  11.5 ± 0.71 ",
-    fixed = TRUE
+  expect_output(
+    print(x_with_chains, color = FALSE),
+    paste0(
+      "rvar<1,2>\\[2,3\\] mean . sd:\n",
+      "     \\[,1\\]         \\[,2\\]         \\[,3\\]        \\n",
+      "\\[1,\\]  1.5 . 0.71   5.5 . 0.71   9.5 . 0.71 \n",
+      "\\[2,\\]  3.5 . 0.71   7.5 . 0.71  11.5 . 0.71"
+    )
   )
 })
 
 test_that("basic str.rvar works", {
-  skip_on_os("windows")
   x <- rvar(array(1:24, dim = c(2,3,4)))
   x_with_chains <- rvar(array(1:24, dim = c(2,3,4)), nchains = 2)
 
   expect_output(str(rvar()),
-    " rvar<1>[0] ",
-    fixed = TRUE
+    " rvar<1>\\[0\\] "
   )
   expect_output(str(rvar(1:3)),
-    " rvar<3>[1]  2 ± 1",
-    fixed = TRUE
+    " rvar<3>\\[1\\]  2 . 1"
   )
   expect_output(str(x, vec.len = 5),
-    " rvar<2>[3,4]  1.5 ± 0.71  3.5 ± 0.71  5.5 ± 0.71  7.5 ± 0.71  9.5 ± 0.71 ...",
-    fixed = TRUE
+    " rvar<2>\\[3,4\\]  1.5 . 0.71  3.5 . 0.71  5.5 . 0.71  7.5 . 0.71  9.5 . 0.71 ..."
   )
   expect_output(str(x_with_chains, vec.len = 5),
-    " rvar<1,2>[3,4]  1.5 ± 0.71  3.5 ± 0.71  5.5 ± 0.71  7.5 ± 0.71  9.5 ± 0.71 ...",
-    fixed = TRUE
+    " rvar<1,2>\\[3,4\\]  1.5 . 0.71  3.5 . 0.71  5.5 . 0.71  7.5 . 0.71  9.5 . 0.71 ..."
   )
 
   x_with_attrs <- x
@@ -53,23 +50,18 @@ test_that("basic str.rvar works", {
   attr(draws_of(x_with_attrs), "foo") = list(1,2)
   attr(x_with_attrs, "bar") = list(1,2)
 
-  expect_output(str(x_with_attrs, vec.len = 5),
-    ' rvar<2>[3,4]  1.5 ± 0.71  3.5 ± 0.71  5.5 ± 0.71  7.5 ± 0.71  9.5 ± 0.71 ...
- - dimnames(*)=List of 2
-  ..$ : chr [1:3] "a" "b" "c"
-  ..$ : NULL
- - attr(draws_of(*), "foo")=List of 2
-  ..$ : num 1
-  ..$ : num 2
- - attr(*, "bar")=List of 2
-  ..$ : num 1
-  ..$ : num 2',
-    fixed = TRUE
+  expect_output(
+    str(x_with_attrs, vec.len = 5),
+    paste0(
+      " rvar<2>\\[3,4\\]  1.5 . 0.71  3.5 . 0.71  5.5 . 0.71  7.5 . 0.71  9.5 . 0.71 ...\n",
+      " - dimnames\\(\\*\\)=List of 2\n",
+      '  \\.\\.\\$ : chr \\[1:3\\] "a" "b" "c"'
+    )
   )
-
 })
 
 test_that("glimpse on rvar works", {
+  skip_on_cran()
   x_vec <- rvar(array(1:24, dim = c(6,4)))
   x_matrix <- rvar(array(1:24, dim = c(2,3,4)))
 
