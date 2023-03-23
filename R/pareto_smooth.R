9ca1c1a03ffc7c5686b13a6a9817345e7dec010f
n-kall <n-kall@github.com>
2023-03-23 13:27:53 +0200

    Begin cleanup of Pareto-smoothing functions

--- R/pareto_smooth.R
+++ R/pareto_smooth.R
@@ -1,18 +1,35 @@
+##' Pareto-smoothing minimum sample-size
+##'
 #' Given Pareto-k computes the minimum sample size for reliable Pareto
 #' smoothed estimate (to have small probability of large error)
+##' @param k
+##' @return minimum sample size
 ps_min_ss <- function(k) {
   # Eq (11) in PSIS paper
   10^(1 / (1 - max(0, k)))
 }
 
-#' Given sample size S computes khat threshold for reliable Pareto
-#' smoothed estimate (to have small probability of large error)
+
+
+##' Pareto-smoothing k-hat threshold
+##'
+##' Given sample size S computes khat threshold for reliable Pareto
+##' smoothed estimate (to have small probability of large error)
+##' @param S sample size
+##' @return threshold
 ps_khat_threshold <- function(S) {
   1 - 1 / log10(S)
 }
 
-#' Given S and scalar or array of k's, compute the relative
-#' convergence rate of PSIS estimate RMSE
+
+
+##' Pareto-smoothing convergence rate
+##'
+##' Given S and scalar or array of k's, compute the relative
+##' convergence rate of PSIS estimate RMSE
+##' @param k pareto-k values
+##' @param S sample size
+##' @return convergence rate
 ps_convergence_rate <- function(k, S) {
   # allow array of k's
   rate <- numeric(length(k))
@@ -33,7 +50,13 @@ ps_convergence_rate <- function(k, S) {
   rate
 }
 
-#' Given S and scalar and k, form a diagnostic message string
+
+##' Pareto-k diagnostic message
+##'
+##' Given S and scalar and k, form a diagnostic message string
+##' @param k pareto-k values
+##' @param S sample size
+##' @return diagnostic message
 pareto_k_diagmsg <- function(k, S) {
   msg <- paste0('With k=', round(k,2), ' and S=', round(S,0), ':\n')
   if (k > 1) {
@@ -42,10 +65,10 @@ pareto_k_diagmsg <- function(k, S) {
     if (k > ps_khat_threshold(S)) {
       msg <- paste0(msg, 'S is too small, and sample size larger than ', round(ps_min_ss(k),0), ' is neeeded for reliable results.\n')
     } else {
-      msg <- paste0(msg, 'To half the RMSE, approximately ', round(2^(2/ps_convergence_rate(k,S)),1), ' times bigger S is needed.\n')
+      msg <- paste0(msg, 'To halve the RMSE, approximately ', round(2^(2/ps_convergence_rate(k,S)),1), ' times bigger S is needed.\n')
     }
   }
-  if (k > 0.7 & k < 1) {
+  if (k > 0.7 && k < 1) {
     msg <- paste0(msg, 'Bias dominates RMSE, and the variance based MCSE is underestimated.\n')
   }
   message(msg)
@@ -58,17 +81,46 @@ pareto_k_diagmsg <- function(k, S) {
 # Question:
 #   - if draws is a draws matrix or similar object with multiple parameters
 #   and r_eff is a vector, then how to structure the output?
-pareto_khat <- function(draws,
-                        ndraws_tail = ceiling(3 * sqrt(length(draws))),
-                        tail = c("right", "left", "both"),
-                        r_eff = 1,
-                        diagmsg = TRUE) {
+
+##' Pareto-khat
+##'
+##' Calculate pareto-khat value given draws
+##' @param draws
+##' @param ndraws_tail number of draws for the tail. Default is max(ceiling(3 * sqrt(length(draws))), S / 5)
+##' @param tail which tail
+##' @param r_eff relative effective. Default is "auto"
+##' @param diagmsg display diagnostic message?
+##' @return list
+pareto_khat <- function(x, ...) {
+  UseMethod("pareto_khat")
+}
+
+pareto_khat.default <- function(x, ...) {
+  x <- as_draws(x)
+  pareto_khat(x, ...)
+}
+
+
+pareto_khat.draws <- function(x,
+                              ndraws_tail = "default",
+                              tail = c("right", "left", "both"),
+                              r_eff = NULL,
+                              diagmsg = TRUE) {
+
+  if (is.null(r_eff) {
+    r_eff <- 1 # TODO: calculate this
+  }
+
+  S <- ndraws(x)
+
+  if (ndraws_tail == "default") {
+    ndraws_tail <- max(ceiling(3 * sqrt(length(x))), S / 5)
+  }
 
   tail <- match.arg(tail)
-  S <- length(draws)
 
   if (ndraws_tail >= 5) {
-    ord <- sort.int(draws, index.return = TRUE)
+    ord <- sort.int(x, index.return = TRUE)
     tail_ids <- seq(S - ndraws_tail + 1, S)
     draws_tail <- ord$x[tail_ids]
     if (abs(max(draws_tail) - min(draws_tail)) < .Machine$double.eps / 100) {
@@ -89,7 +141,7 @@ pareto_khat <- function(draws,
       } else {
         tail <- draws_tail
       }
-      draws[ord$ix[tail_ids]] <- tail
+      x[ord$ix[tail_ids]] <- tail
     }
   } else {
     warning(
@@ -100,7 +152,7 @@ pareto_khat <- function(draws,
   }
 
   # truncate at max of raw draws
-  draws[draws > ord$x[S]] <- ord$x[S]
+  x[x > ord$x[S]] <- ord$x[S]
 
   min_ss <- ps_min_ss(k)
 
@@ -119,5 +171,4 @@ pareto_khat <- function(draws,
     "khat_threshold" = khat_threshold,
     "convergence_rate" = convergence_rate
   )
-
 }
