cae2893c2d68653177cc9182c4f8de039ae78e9b
n-kall <n-kall@github.com>
2023-04-27 10:42:11 +0300

    fix documentation for ndraws_tail in pareto smoothing functions

--- man-roxygen/args-pareto.R
+++ man-roxygen/args-pareto.R
@@ -10,7 +10,8 @@
 #' The default is `"both"`.
 #' @param ndraws_tail (numeric) number of draws for the tail. If
 #'   `ndraws_tail` is not specified, it will be calculated as
-#'   max(ceiling(3 * sqrt(length(x))), length(x) / 5).
+#'   ceiling(3 * sqrt(length(x) / r_eff)) if length(x) > 225 and
+#'   length(x) / 5 otherwise (see Appendix H in Vehtari et al. (2022)).
 #' @param r_eff (numeric) relative effective sample size estimate. If
 #'   `r_eff` is omitted, it will be calculated assuming the draws are
 #'   from MCMC.
