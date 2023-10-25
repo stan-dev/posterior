17bf7575e43d499518d50224b6907e45dc4c775e
n-kall <n-kall@github.com>
2023-10-25 15:32:00 +0300

    cleanup rhat nested tests

--- tests/testthat/test-rhat_nested.R
+++ tests/testthat/test-rhat_nested.R
@@ -1,11 +1,11 @@
 test_that("rhat_nested returns reasonable values", {
   tau <- extract_variable_matrix(example_draws(), "tau")
 
-  rhat <- rhat_nested(tau, superchain_ids = c(1, 2, 1, 2))
-  expect_true(rhat > 0.99 & rhat < 1.05)
+  nested_rhat <- rhat_nested(tau, superchain_ids = c(1, 2, 1, 2))
+  expect_true(nested_rhat > 1 & nested_rhat < 1.05)
 
-  rhat <- rhat_nested(tau, superchain_ids = c(1, 2, 1, 2))
-  expect_true(rhat > 0.99 & rhat < 1.05)
+  nested_rhat <- rhat_nested(tau, superchain_ids = c(1, 2, 1, 2))
+  expect_true(nested_rhat > 1 & nested_rhat < 1.05)
 })
 
 
@@ -15,17 +15,17 @@ test_that("rhat_nested handles special cases correctly", {
   expect_true(is.na(rhat_nested(x, superchain_ids = c(1))))
 
   x <- c(rnorm(10), Inf)
-  expect_true(is.na(rhat_nested(x, superchain_ids = c(1,2,1,2))))
+  expect_true(is.na(rhat_nested(x, superchain_ids = c(1, 2, 1, 2))))
 
   tau <- extract_variable_matrix(example_draws(), "tau")
   expect_warning(
-    rhat_nested(tau, superchain_ids = c(1,1,1,3)),
+    rhat_nested(tau, superchain_ids = c(1, 1, 1, 3)),
     "Number of chains per superchain is not the same for each superchain, returning NA."
   )
 
   tau <- extract_variable_matrix(example_draws(), "tau")
   expect_warning(
-    rhat_nested(tau, superchain_ids = c(1,2)),
+    rhat_nested(tau, superchain_ids = c(1, 2)),
     "Length of superchain_ids not equal to number of chains, returning NA."
   )
 })
