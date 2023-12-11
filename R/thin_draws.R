3530c9941eb7f732b421b597f0d0ca4c9291e6bd
n-kall <n-kall@github.com>
2023-12-11 12:12:48 +0200

    add automatic thinning of draws

--- R/thin_draws.R
+++ R/thin_draws.R
@@ -4,8 +4,11 @@
 #'
 #' @aliases thin
 #' @template args-methods-x
-#' @param thin (positive integer) The period for selecting draws.
+#' @param thin (positive integer) The period for selecting draws. If
+#'   omitted, this will be automatically calculated based on bulk-ESS
+#'   and tail-ESS as suggested by Säilynoja et al. (2022).
 #' @template args-methods-dots
+#' @template ref-sailynoja-ecdf-2022
 #' @template return-draws
 #'
 #' @examples
@@ -16,13 +19,18 @@
 #' niterations(x)
 #'
 #' @export
-thin_draws <- function(x, thin, ...) {
+thin_draws <- function(x, thin = NULL, ...) {
   UseMethod("thin_draws")
 }
 
 #' @rdname thin_draws
 #' @export
-thin_draws.draws <- function(x, thin, ...) {
+thin_draws.draws <- function(x, thin = NULL, ...) {
+  if (is.null(thin)) {
+    thin <- round(ess_based_thinning_all_vars(x))
+    message("Automatically thinned by ", round(thin), " based on ESS.")
+  }
+
   thin <- as_one_integer(thin)
   if (thin == 1L) {
     # no thinning requested
@@ -32,7 +40,7 @@ thin_draws.draws <- function(x, thin, ...) {
     stop_no_call("'thin' must be a positive integer.")
   }
   niterations <- niterations(x)
-  if (thin > niterations ) {
+  if (thin > niterations) {
     stop_no_call("'thin' must be smaller than the total number of iterations.")
   }
   iteration_ids <- seq(1, niterations, by = thin)
@@ -41,6 +49,20 @@ thin_draws.draws <- function(x, thin, ...) {
 
 #' @rdname thin_draws
 #' @export
-thin_draws.rvar <- function(x, thin, ...) {
+thin_draws.rvar <- function(x, thin = NULL, ...) {
   thin_draws(draws_rvars(x = x), thin, ...)$x
+}
+
+ess_based_thinning_all_vars <- function(x, ...) {
+  max(summarise_draws(x, thin = ess_based_thinning)$thin)
+}
+
+ess_based_thinning <- function(x, ...) {
+  # thin based on mean (over chains) of minimum of tail and bulk ess
+  x <- as.matrix(x)
+  ess_tailbulk_chains <- apply(x,
+    MARGIN = 2,
+    FUN = function(x) min(SW(ess_tail(x)), SW(ess_bulk(x)))
+    )
+  nrow(x) / mean(ess_tailbulk_chains)
 }
---
+++ man-roxygen/ref-sailynoja-ecdf-2022.R
@@ -0,0 +1,5 @@
+#' @references
+#' Teemu Säilynoja, Paul-Christian Bürkner, and Aki Vehtari (2022).
+#' Graphical test for discrete uniformity and its applications in
+#' goodness-of-fit evaluation and multiple sample comparison.
+#' *Statistics and Computing*. 32, 32. doi:10.1007/s11222-022-10090-6
