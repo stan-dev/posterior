0e755bc220c0e862b3e568dd670876ac68ecd01c
VisruthSK <public@visruth.com>
2026-06-17 23:36:42 -0700

    Small fixes

--- src/init.c
+++ src/init.c
@@ -3,10 +3,6 @@
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
-/* FIXME: 
-   Check these declarations against the C/Fortran source code.
-*/
-
 /* .Call calls */
 extern SEXP posterior_ps_tail_select(SEXP, SEXP);
 
