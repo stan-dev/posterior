1fe26fdf81f4577743f22274f118b1b2687108e8
Herman Tesso <161074145+hermanFTT@users.noreply.github.com>
2026-08-31 21:51:07 +0300

    polish style
    Co-authored-by: Florence Bockting <48919471+florence-bockting@users.noreply.github.com>

--- tests/testthat/test-uniformity_test.R
+++ tests/testthat/test-uniformity_test.R
@@ -52,7 +52,6 @@ test_that("uniformity_test is calibrated", {
   }
 })
 
-
 # Tests for the dependence-aware uniformity tests ------------------------------
 test_that("piet_test computes correct p-values", {
   x <- c(0.1, 0.25, 0.5, 0.75, 0.9)
