ec0dc47cdc87001770d7e40eaadb45b14e5459ac
Herman Tesso <161074145+hermanFTT@users.noreply.github.com>
2026-08-31 21:55:49 +0300

    polish style
    Co-authored-by: Florence Bockting <48919471+florence-bockting@users.noreply.github.com>

--- tests/testthat/test-uniformity_test.R
+++ tests/testthat/test-uniformity_test.R
@@ -224,3 +224,4 @@ test_that("compute_cauchy computes correct transformations", {
   # For x = 0.75: tan((0.5 - 0.75) * pi) = tan(-0.25 * pi) = -1
   expect_equal(.compute_cauchy(0.75), -1, tolerance = 1e-10)
 })
+
